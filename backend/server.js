84329a0128818ed2beea0903e04070853a100285
Kritika <kritikasinghsam@gmail.com>
2026-09-22 14:36:31 +0530

    Fix session cookie for production

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,17 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
+  // cookie: {
+  //   secure: false,
+  //   httpOnly: true,
+  //   maxAge: 8 * 60 * 60 * 1000
+  // }
   cookie: {
-    secure: false,
-    httpOnly: true,
-    maxAge: 8 * 60 * 60 * 1000
-  }
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+  httpOnly: true,
+  maxAge: 8 * 60 * 60 * 1000
+}
 }));
 
 app.use('/oauth', oauthRoutes);
